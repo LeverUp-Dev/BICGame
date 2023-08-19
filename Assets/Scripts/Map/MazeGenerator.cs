be2af7ddb9941a52952ef6087ed2667aa997893f
phiprip-Choi <132041240+phiprip-Choi@users.noreply.github.com>
2023-08-19 20:02:22 +0900

    미로 출입구 무작위 배치 및 코드 간소화

--- Assets/Scripts/Map/MazeGenerator.cs
+++ Assets/Scripts/Map/MazeGenerator.cs
@@ -78,39 +78,33 @@ namespace Hypocrites.Maze
             mazeWallMap[x, y] = Directions.RIGHT; // 처음 자리 잡은 방에 방문 표시
             while (!isEnd)
             {
-                bool[] isBlock = new bool[4];
-                bool isRemain = false;
-                while (!isAllTrue(isBlock))
+                while (true)
                 {
-                    int prevX = x, prevY = y;
-                    int randIndex = Random.Range(0, 4);
-                    Directions r = (Directions)(1 << randIndex);
-                    switch (r)
-                    {
-                        case Directions.DOWN: // 2
-                            if (MAZE_MAP_SIZE == y + 1 || mazeWallMap[x, y + 1] != Directions.NONE || StartRange(x, y + 2)) isBlock[randIndex] = true;
-                            else y++;
-                            break;
-                        case Directions.RIGHT: // 1
-                            if (MAZE_MAP_SIZE == x + 1 || mazeWallMap[x + 1, y] != Directions.NONE || StartRange(x + 2, y)) isBlock[randIndex] = true;
-                            else x++;
-                            break;
-                        case Directions.UP: // 0
-                            if (0 > y - 1 || mazeWallMap[x, y - 1] != Directions.NONE || StartRange(x, y - 2)) isBlock[randIndex] = true;
-                            else y--;
-                            break;
-                        case Directions.LEFT: // 3
-                            if (0 > x - 1 || mazeWallMap[x - 1, y] != Directions.NONE || StartRange(x - 2, y)) isBlock[randIndex] = true;
-                            else x--;
-                            break;
-                    }
-                    if (isBlock[randIndex]) continue;
+                    List<Directions> dir = new List<Directions>();
+                    if (MAZE_MAP_SIZE != y + 1 && mazeWallMap[x, y + 1] == Directions.NONE && !StartRange(x, y + 2)) dir.Add(Directions.DOWN);
+                    if (MAZE_MAP_SIZE != x + 1 && mazeWallMap[x + 1, y] == Directions.NONE && !StartRange(x + 2, y)) dir.Add(Directions.RIGHT);
+                    if (0 <= y - 1 && mazeWallMap[x, y - 1] == Directions.NONE && !StartRange(x, y - 2)) dir.Add(Directions.UP);
+                    if (0 <= x - 1 && mazeWallMap[x - 1, y] == Directions.NONE && !StartRange(x - 2, y)) dir.Add(Directions.LEFT);
 
-                    Clear(isBlock);
-                    mazeWallMap[prevX, prevY] |= r; // 원래 방문 벽 허물기
-                    mazeWallMap[x, y] |= r.GetOppositeDirection(); //이동할 방향
+                    if (dir.Count != 0)
+                    {
+                        int prevX = x, prevY = y;
+                        int randIndex = Random.Range(0, dir.Count);
+                        Directions r = dir[randIndex];
+                        switch (r)
+                        {
+                            case Directions.DOWN: y++; break;
+                            case Directions.RIGHT: x++; break;
+                            case Directions.UP: y--; break;
+                            case Directions.LEFT: x--; break;
+                        }
+                        mazeWallMap[prevX, prevY] |= r; // 원래 방문 벽 허물기
+                        mazeWallMap[x, y] |= r.GetOppositeDirection(); //이동할 방향
+                    }
+                    else break;
                 }
 
+                bool isRemain = false;
                 for (int yPos = 0; yPos < MAZE_MAP_SIZE; yPos++)
                 {
                     for (int xPos = 0; xPos < MAZE_MAP_SIZE; xPos++)
@@ -162,20 +156,43 @@ namespace Hypocrites.Maze
              * 1. mazeWallMap은 mazeWalls보다 크기가 1/2 - 1 만큼 작으므로 주의
              * 2. 2차원 배열의 왼쪽 맨 아래부터 위-오른쪽으로 순회하므로 순서 주의
              */
+
+            int xStart = mazeCenter, yStart = mazeCenter;
+            Directions randDirStart = (Directions)(1 << Random.Range(0, 4));
+            switch (randDirStart)
+            {
+                case Directions.RIGHT:
+                case Directions.LEFT:
+                    xStart += StartSize * ((randDirStart == Directions.RIGHT) ? 1 : -1) + 1;
+                    yStart += Random.Range(-StartSize + 1, StartSize);
+                    break;
+                case Directions.DOWN:
+                case Directions.UP:
+                    xStart += Random.Range(-StartSize + 1, StartSize);
+                    yStart += StartSize * ((randDirStart == Directions.DOWN) ? 1 : -1);
+                    break;
+            }
             for (int i = 0; i < mapSize; i++)
             {
                 for (int j = 0; j < mapSize; j++)
                 {
                     int x = (i - 1) / 2;
                     int y = (mapSize - j - 1) / 2;
-                    bool startRangeUnblock = x > mazeCenter - StartSize && x < mazeCenter + StartSize &&
-                        y > mazeCenter - StartSize + 1 && y <= mazeCenter + StartSize;
+                    bool odd = i % 2 == 1 && j % 2 == 1;
+                    bool startRangeUnblock = x > mazeCenter - StartSize && x < mazeCenter + StartSize && y > mazeCenter - StartSize + 1 && y <= mazeCenter + StartSize;
 
                     var mazeHalfSize = new Vector3(mapSize, 0, mapSize) / 2;
                     var wallPosition = new Vector3(i, 0, j) - mazeHalfSize + mapTransform.position + Vector3.left * 0.5f;
                     var floorPosition = new Vector3(i, 0, j) - mazeHalfSize + mapTransform.position + Vector3.down * 0.1f;
 
                     Object.Instantiate(floorPrefab, floorPosition, Quaternion.identity, hierarchyRoot); // 바닥 형성
+
+                    // 방의 출입구 무작위 배치
+                    if (x == xStart && y == yStart && odd)
+                    {
+                        if (randDirStart == Directions.RIGHT || randDirStart == Directions.LEFT) Object.Destroy(mazeWalls[i - 1, mapSize - j - 1]); //가로
+                        else Object.Destroy(mazeWalls[i, mapSize - j]); // 세로
+                    }
                     // 외벽에 네 구역과 연결하기 위해 벽 허물기
                     if (i == 0 || i == mapSize - 1 || j == 0 || j == mapSize - 1)
                     {
@@ -183,24 +200,22 @@ namespace Hypocrites.Maze
                             continue;
                     }
 
-                    if (i > 0 && i < mapSize && j > 0 && j < mapSize)
+                    if (startRangeUnblock) continue;
+                    else if (odd)
                     {
-                        if (startRangeUnblock) continue;
-                        else if (i % 2 == 1 && j % 2 == 1)
-                        {
-                            /* 벽 허물기 (왼쪽 맨 아래부터 위-오른쪽으로 순회) */
+                        /* 벽 허물기 (왼쪽 맨 아래부터 위-오른쪽으로 순회) */
 
-                            Directions crashDirections = mazeWallMap[x, y];
-                            
-                            if (crashDirections.Contains(Directions.LEFT) || mazeWallMap[x, y] == Directions.NONE)
-                                Object.Destroy(mazeWalls[i - 1, mapSize - j - 1]);
+                        Directions crashDirections = mazeWallMap[x, y];
 
-                            if (crashDirections.Contains(Directions.DOWN) || mazeWallMap[x, y] == Directions.NONE)
-                                Object.Destroy(mazeWalls[i, mapSize - j]);
+                        if (crashDirections.Contains(Directions.LEFT) || (mazeWallMap[x, y] == Directions.NONE && x != mazeCenter - StartSize + 1))
+                            Object.Destroy(mazeWalls[i - 1, mapSize - j - 1]);
 
-                            continue;
-                        }
+                        if (crashDirections.Contains(Directions.DOWN) || (mazeWallMap[x, y] == Directions.NONE && y != mazeCenter + StartSize))
+                            Object.Destroy(mazeWalls[i, mapSize - j]);
+
+                        continue;
                     }
+
 
                     mazeWalls[i, mapSize - j - 1] = Object.Instantiate(wallPrefab, wallPosition, Quaternion.identity, hierarchyRoot);
                 }
